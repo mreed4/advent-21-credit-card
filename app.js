f61ca9e652e87112fff8a1cae98b9fcc4fc22938
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-21 09:54:14 -0800

    Additional functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const creditCard = document.querySelector(".credit-card__wrapper");
 const form = document.querySelector("form");
+const submitBtn = form.querySelector("button");
 
 (function init() {
   form.addEventListener("submit", (e) => {
@@ -12,7 +13,44 @@ const form = document.querySelector("form");
   monthInputListener();
   yearInputListener();
   cvvInputListener();
+
+  validateFields();
 })();
+
+function validateFields() {
+  const inputs = [...form.querySelectorAll("input")];
+  const selects = [...form.querySelectorAll("select")];
+
+  const allFields = [...inputs, ...selects];
+
+  allFields.forEach((field) => {
+    field.addEventListener("blur", (e) => {
+      const fieldsToBool = allFields.map((field) => {
+        const { value, type } = field;
+
+        if (type === "text" && !value) {
+          // field.classList.add("error");
+          return false;
+        }
+
+        if (type === "select-one" && (value === "Month" || value === "Year")) {
+          // field.classList.add("error");
+          return false;
+        }
+
+        return true;
+      });
+
+      const allFieldsValid = fieldsToBool.every((field) => field);
+
+      console.log(fieldsToBool, allFieldsValid);
+
+      if (allFieldsValid) {
+        submitBtn.removeAttribute("disabled");
+      }
+    });
+  });
+}
 
 function cvvInputListener() {
   const cvvInput = document.querySelector("form #cvv");
@@ -22,19 +60,14 @@ function cvvInputListener() {
     creditCard.classList.add("flip");
   });
 
-  cvvInput.addEventListener("blur", () => {
+  cvvInput.addEventListener("blur", (e) => {
     creditCard.classList.remove("flip");
   });
 
   cvvInput.addEventListener("input", (e) => {
     const { value } = e.target;
 
-    const regex = /^[0-9]{3,4}$/;
-
-    if (!regex.test(value)) {
-      cvvInput.classList.add("error");
-      return;
-    }
+    cvvInput.classList.remove("error");
 
     if (value.length > 3) return;
 
@@ -47,9 +80,20 @@ function cardNumberInputListener() {
   const cardNumber = document.querySelector(".card-number").children;
   const [numberShadow, numberEmboss] = cardNumber;
 
+  cardNumberInput.addEventListener("keydown", (e) => {
+    if (!/\d/.test(e.key) && e.key !== "Backspace" && e.key !== "Tab") {
+      e.preventDefault();
+      console.log("Numbers only");
+    }
+  });
+
   cardNumberInput.addEventListener("input", (e) => {
     /* */
     const { value } = e.target;
+
+    if (value.length === 19) {
+      cardNumberInput.classList.remove("error");
+    }
 
     if (value[0] === "3") {
       creditCard.classList.remove("visa", "mastercard", "discover");
@@ -81,8 +125,8 @@ function cardNumberInputListener() {
 
     cardNumberInput.value = formattedValue;
 
-    numberEmboss.textContent = formattedValue;
-    numberShadow.textContent = formattedValue;
+    numberEmboss.textContent = formattedValue ? formattedValue : "4242 4242 4242 4242";
+    numberShadow.textContent = formattedValue ? formattedValue : "4242 4242 4242 4242";
   });
 }
 
